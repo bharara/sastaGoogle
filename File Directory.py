d16534027f2e0de637c25ddabfa23e291b8a9a3a
bharara <32545366+bharara@users.noreply.github.com>
2018-12-11 21:02:36 +0500

    Update File Directory.py

--- File Directory.py
+++ File Directory.py
@@ -1,11 +1,19 @@
 import os, os.path
 from pathlib import Path
-filename="C:/Users/USER/Desktop/Trip Murree 2nd Semester"
-arrayofpath=Path(filename).glob("**/*")
+
+def printInFolder(files):
+	n = 1
+	igone = ('Category~','Image~','Talk~','Template~','Template_talk~','User~','User_talk~')
+	for i in files:
+		i = str(i)[40:]
+		if i.startswith(igone):
+			continue
+		print(n,i)
+		n+=1
+
+filename = "D:/3- DSA/Project/simple//articles/u/n/i/"
+arrayofpath = Path (filename).glob("**/*")
 
 files=[x for x in arrayofpath if x.is_file()]
-#print(len(files))
-for i in files:
-    print(i)
-
-
+#print(len(filename))
+printInFolder(files)
